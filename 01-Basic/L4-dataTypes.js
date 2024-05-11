a90d313f9fb07d0964a95516da388bcb18a41064
BHARAT KUMAR <112845975+cs-bharat@users.noreply.github.com>
2024-05-11 19:30:33 +0000

    data type

---
+++ 01-Basic/L4-dataTypes.js
@@ -0,0 +1,23 @@
+"use stript";
+// readability of code is also high
+
+
+let name = "Viswa"
+let age = 18
+let loginned = true
+let state;
+
+  /* premitive data type 
+// name = "string"
+//  number => range 2 to power 53
+// boolean = false/true
+// null => standalone value
+let state;   // undefined 
+// let symbol => unique nes define.
+
+*/
+
+console.log(typeof "bharat") // string
+console.log(typeof age) // number
+console.log(typeof null)// object
+console.log(typeof undefined);  // undefined 
