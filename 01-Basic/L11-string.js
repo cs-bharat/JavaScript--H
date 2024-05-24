0d8b024c2537a3f1150f35b13753f3b33014954f
BHARAT KUMAR <112845975+cs-bharat@users.noreply.github.com>
2024-05-24 19:37:47 +0000

    string done

---
+++ 01-Basic/L11-string.js
@@ -0,0 +1,33 @@
+const country = 'India'
+const state = 'Rajasthan'
+console.log(`Hello welcome to ${country} and best state in india is ${state}`)
+
+// another way to create string //
+const city  = new String('Udaipur-rajasthan-india')
+console.log(city[0]);
+console.log(city.length);
+
+// prototypes ke under ki method used //
+console.log(city.toUpperCase());
+console.log(city.indexOf('p'));
+
+
+const anotherCity = city.substring(0,7) // last se one latter skip.
+console.log(anotherCity);
+
+const anotherCity2 = city.slice(0,4) // negative value also posible
+console.log(anotherCity2);
+
+const anotherCity3 = city.slice(-16,-4);
+console.log(anotherCity3);
+
+const newStringCity  = '   space    ' 
+console.log(newStringCity);
+console.log(newStringCity.trim()); // string ke aage or piche ke space ko remove kar deta he
+
+const url = 'https://hitesh.com/bharat%20kumar'
+console.log(url.replace('%20' , '-'))
+console.log(url.includes('india'));
+
+// split method 
+console.log(city.split("-"));
