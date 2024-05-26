f860e043ebe008877a4612f6993aacf1905e6062
BHARAT KUMAR <112845975+cs-bharat@users.noreply.github.com>
2024-05-26 06:59:13 +0000

    date and time done

--- 01-Basic/L13_datasinJs.js
+++ 01-Basic/L13_datasinJs.js
@@ -1,9 +1,9 @@
 let myDate = new Date()
-console.log(myDate);
-console.log(myDate.toString());
+// console.log(myDate);
+// console.log(myDate.toString());
 console.log(myDate.toLocaleString());
-console.log(myDate.toLocaleDateString());
-console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toDateString());
 
 // interview
 console.log(typeof myDate); // object
@@ -13,3 +13,15 @@ console.log(myCreateDate.toLocaleString());
 
 const myCreateDate2 = new Date("06-19-2004") // mm-dd-yy
 console.log(myCreateDate2.toLocaleString());
+
+
+// hotel website ya pooling banay tab//
+let myTimeStamp = Date.now();
+console.log(myTimeStamp); // milisecond value
+console.log(Math.floor(Date.now()/1000));
+
+// (``) ==> string interpolation kehete he //
+
+myDate.toLocaleString('default' , {
+    weekday:"long"
+})
