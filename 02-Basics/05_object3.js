b6295ea81c88d20a520689df995144ca03683061
BHARAT KUMAR <112845975+cs-bharat@users.noreply.github.com>
2024-05-30 07:29:03 +0000

    json and object destructoring

---
+++ 02-Basics/05_object3.js
@@ -0,0 +1,22 @@
+// object de-structaring and apis //
+
+const cource = {
+    courceName : 'js hindi',
+    courceFee : 999,
+    courceInstructor : 'Hitesh Choudhary ',
+    courceDuration : '2 month',
+}
+
+// access object value than .//
+// console.log(cource.courceInstructor);
+
+// how to used de-structuring  {} used to de-structuring //
+const {courceInstructor:instructor} = cource;
+console.log(instructor);
+
+
+/* apis ka response = json formate me ya array me aata
+  or aagr apis response study (jsonformater) ka use kar sakte to study
+   api response data   */
+
+   // best api to learning -> (randomeuser.me) //
