5661bef3f5040d7b70b2b0a528094b8f6605ad2b
BHARAT KUMAR <112845975+cs-bharat@users.noreply.github.com>
2024-06-13 19:38:32 +0000

    async / synchronus

---
+++ Async/async.js
@@ -0,0 +1,37 @@
+// synchronous and asyncronous //
+
+/*
+1- synchronous  => block the flow of program 
+example => App yahi per bethiye kuch mat kariye me appke liye water 
+           lekar aata hu .(flow of program block..)
+
+2- asynchronous => does not block execution 
+example => app yahi per bethiye mobile chalaiye me aapke liye chai
+           banakar lata hu.(app aapna kam karo bethe mat raho.)
+ */
+
+
+           // javascript engine //
+        
+  /*
+   javascript engine -> ye engine do chijo se milkar bana hota he 
+                        1) heap memory 2) call-stack 
+
+                        webAPI -> ye Browser me hi available hoti he. DOM api available 
+                        NODE -> run time Environment (DOM api) not available 
+
+   */      
+  
+                        /* 
+                          setTimeOut(1) , setTimeInterval(2)
+                          1-> setTimeOut - async function he jo ki kuch time 
+                                           ka delay mangta he or useke bad execute
+                                           ho jata he.
+
+                         2-> setTimeInterval - async function he jo ki kuch time 
+                                                ka delay hi mangta he or useke bad
+                                                continue execute hota hi rehta he. 
+                                                infinite times.                  
+
+                        */
+                                                         
